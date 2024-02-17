b89226303843403fe5ebdd02658008c73e4bad8f
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-02-17 15:20:22 +0530

    Node and DB Connection
    Node modules installation , Create App.js and Create a connection with MongoDB

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import express from "express"
+import {logger} from "./utills/loggerfile"
+import cors from "cors"
+import MongoConnect from "./configs/DB_Connection"
+import "dotenv/config"
+
+
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+
+app.use(express.json({limit:'20mb'}))
+
+app.use((req,res,next) => {
+    logger.warn(req.method,req.path)
+    next()
+})
+
+app.listen(PORT , ()=>{
+    logger.info("Connected via Port " + PORT)
+    MongoConnect()
+    
+})
+
+
+
