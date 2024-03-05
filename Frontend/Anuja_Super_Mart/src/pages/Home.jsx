50b43477fc73fc91c1869359c310d15c483a22e0
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-03-05 09:49:22 +0530

    Add Pages and more dependencies
    Add Some pages for start work
    Add "@popperjs/core": "^2.11.8",
     "react-router-dom": "^6.22.2",
    
    dependencies

---
+++ Frontend/Anuja_Super_Mart/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+
+import Navbar from "../components/Nav";
+
+const Home = () => {
+    return ( 
+       <>
+        <Navbar/>
+        <div className="Hero">
+            
+            <h1>Your Title Goes Here</h1>
+            <p>Your description or tagline goes here.</p>
+            <a href="#learn-more" class="btn">Learn More</a>
+        </div>
+       </>
+     );
+}
+ 
+export default Home;
