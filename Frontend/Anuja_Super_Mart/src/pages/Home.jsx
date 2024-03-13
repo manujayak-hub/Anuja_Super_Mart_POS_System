5db4e7c061f5b3d59a281694adb00be06465334e
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-03-13 17:35:00 +0530

    resolve conflicts in css
    resolve conflicts in css remove conflicted css files

--- Frontend/Anuja_Super_Mart/src/pages/Home.jsx
+++ Frontend/Anuja_Super_Mart/src/pages/Home.jsx
@@ -25,20 +25,17 @@ const Home = () => {
                 <button type="button" class="btn btn-link">Link</button>
 
             </div>
+
+            <Navbar />
+            <div className="Hero">
+
+                <h1>Your Title Goes Here</h1>
+                <p>Your description or tagline goes here.</p>
+
+                <a href="#learn-more" class="btn">Learn More</a>
+            </div>
         </>
     );
-    return ( 
-       <>
-        <Navbar/>
-        <div className="Hero">
-            
-            <h1>Your Title Goes Here</h1>
-            <p>Your description or tagline goes here.</p>
-            
-            <a href="#learn-more" class="btn">Learn More</a>
-        </div>
-       </>
-     );
 
 }
 
