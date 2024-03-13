32d2b66b67327a48446b2d42e5e803819fb05a6c
InduwariHW <154510233+InduwariHW@users.noreply.github.com>
2024-03-13 10:33:14 +0530

    Full specification of the task
    This task folder includes the supplier management form, Supplier Profile and All the information  of the task.

--- Frontend/Anuja_Super_Mart/src/App.jsx
+++ Frontend/Anuja_Super_Mart/src/App.jsx
@@ -4,6 +4,11 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Form from './pages/Task/Form';
+import SProfile from './pages/Task/SProfile';
+import SDetails from './pages/Task/SDetails';
+
+
 import { useStore } from './stores/authStore'
 
 
@@ -19,8 +24,11 @@ function App() {
             <Route index element={<Home />} />
             <Route path="About" element={<About/>} />
             <Route path="contact" element={<Contact/>} />
-            <Route path="Login" element={<Login/>} />
+            <Route path="Login" element={<Login/>}/>
             <Route path="Signup" element={<Signup/>}/>
+            <Route path="Form" element={<Form/>}/>
+            <Route path="SProfile" element={<SProfile/>}/>
+            <Route path="SDetails" element={<SDetails/>}/>
             
             {user && <Route exact path="/About" render={() => <About />} />}
 
