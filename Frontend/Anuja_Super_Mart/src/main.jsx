fb69d3633459fc2705c5312c09d53d85455934b2
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-03-13 10:48:22 +0530

    Bootstrap checked

--- Frontend/Anuja_Super_Mart/src/main.jsx
+++ Frontend/Anuja_Super_Mart/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // Import our custom CSS
-import '../scss/styles.scss'
+import '../src/scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
