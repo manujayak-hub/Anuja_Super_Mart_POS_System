a6b8f77f340a11a7f6e778b29371ac5a1784e6a8
DULAN IMALKA <134499168+XdulanX@users.noreply.github.com>
2024-04-24 17:36:50 +0530

    Update emp_list.jsx
    fixed error

--- Frontend/Anuja_Super_Mart/src/pages/Emp_pages/emp_list.jsx
+++ Frontend/Anuja_Super_Mart/src/pages/Emp_pages/emp_list.jsx
@@ -4,7 +4,8 @@ import EmployeeDetails from './EmployeeDetails';
 import EmployeeForm from './EmployeeForm';
 import AddEmployeeForm from './AddEmployeeForm';
 import SideBar from '../../components/EmployeeComponents/empSideBar';
-import { Modal, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal'; // Correct import for Modal
+import Button from 'react-bootstrap/Button'; // Correct import for Button
 
 const Emp_list = () => {
   const [loading, setLoading] = useState(true);
@@ -98,7 +99,7 @@ const Emp_list = () => {
   return (
     <>
       <style>
-  {`
+        {`
     body {
       background-color: #D8D1D1;
     }
@@ -140,9 +141,9 @@ const Emp_list = () => {
       color: #fff;
     }
   `}
-</style>
+      </style>
 
-      <SideBar/>
+      <SideBar />
       <div className="content" style={{ marginLeft: '250px', marginRight: '50px', padding: '20px' }}>
         <div className="emp_search-bar-container">
           <input
